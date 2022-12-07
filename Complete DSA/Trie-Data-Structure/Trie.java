465924285f8b45743abad6f4f3625b12591a469d
Saad <saadahmedscse@gmail.com>
2022-12-07 23:39:08 +0600

    Implemented search functionality

--- Complete DSA/Trie-Data-Structure/Trie.java
+++ Complete DSA/Trie-Data-Structure/Trie.java
@@ -1,30 +1,53 @@
 public class Trie {
-    class Node {
+    
+    private Node root = new Node();
+
+    private class Node {
+
         Node[] children;
         boolean isEnd;
-
-        private Node root = new Node();
-
+    
         public Node() {
             children = new Node[26]; // for lowercase 'a' to 'z'
             
             for (int i = 0; i < 26; i++) {
                 children[i] = null;
             }
-
+    
             isEnd = false;
         }
+    }
 
-        public void insert(String word) {
-            Node currentNode = root;
+    public void insert(String word) { //Time Complexity O(L) "where 'L' is the length of a word"
+        Node currentNode = root;
+    
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
 
-            for (int i = 0; i < word.length(); i++) {
-                int idx = word.charAt(i) - 'a';
-                if (currentNode.children[idx] == null) {
-                    currentNode.children[idx] = new Node();
-                }
-                currentNode = currentNode.children[idx];
+            if (currentNode.children[idx] == null) {
+                currentNode.children[idx] = new Node();
             }
+
+            if (i == word.length() - 1) {
+                currentNode.children[idx].isEnd = true;
+            }
+
+            currentNode = currentNode.children[idx];
         }
+    }
+
+    public boolean search(String word) {
+        Node currentNode = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+
+            if (currentNode.children[idx] == null) return false;
+            if (i == word.length() - 1 && !currentNode.children[idx].isEnd) return false;
+
+            currentNode = currentNode.children[idx];
+        }
+
+        return true;
     }
 }
