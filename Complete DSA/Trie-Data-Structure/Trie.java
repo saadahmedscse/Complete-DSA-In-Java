739b525ca8ee5dd8590f8dca6e0881a0f04c452b
Saad <saadahmedscse@gmail.com>
2022-12-07 23:16:49 +0600

    Defined node and implemented insert function

--- Complete DSA/Trie-Data-Structure/Trie.java
+++ Complete DSA/Trie-Data-Structure/Trie.java
@@ -1,5 +1,30 @@
 public class Trie {
-    public static void main(String[] args) {
-        
+    class Node {
+        Node[] children;
+        boolean isEnd;
+
+        private Node root = new Node();
+
+        public Node() {
+            children = new Node[26]; // for lowercase 'a' to 'z'
+            
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+
+            isEnd = false;
+        }
+
+        public void insert(String word) {
+            Node currentNode = root;
+
+            for (int i = 0; i < word.length(); i++) {
+                int idx = word.charAt(i) - 'a';
+                if (currentNode.children[idx] == null) {
+                    currentNode.children[idx] = new Node();
+                }
+                currentNode = currentNode.children[idx];
+            }
+        }
     }
 }
