06eede96217061bbba559f77b56dd12efc1dd6a7
Saad <saadahmedscse@gmail.com>
2022-12-12 01:00:38 +0600

    Node class for binary tree

---
+++ Complete DSA/BinaryTree/Node.java
@@ -0,0 +1,11 @@
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
