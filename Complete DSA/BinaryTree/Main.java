84761dc7769d7b344bbe6ebadb2176117f94c686
Saad <saadahmedscse@gmail.com>
2022-12-14 23:39:11 +0600

    Tested floor and ceil

--- Complete DSA/BinaryTree/Main.java
+++ Complete DSA/BinaryTree/Main.java
@@ -7,8 +7,13 @@ public class Main {
         //     3     4  6     7
         //         9
 
+        int[] bstNodes = {15, 10, 5, -1, -1, 12, -1, -1, 20, 17, -1, -1, 25, -1, -1};
+
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
+
+        BinaryTree bst = new BinaryTree();
+        Node bstRoot = bst.buildTree(bstNodes);
 
         //tree.preOrder(root);                          // 1 2 3 4 9 5 6 7
         //tree.inOrder(root);                           // 3 2 9 4 1 6 5 7
@@ -23,9 +28,11 @@ public class Main {
         //tree.rightView(root);                         // 1 5 7 9
         //tree.topView(root);                           // 3 2 1 5 7
         //tree.bottomView(root);                        // 3 9 6 5 7
-        //System.out.println(tree.maxDiameter(root));   // 6 Diameter = {9 -> 4 -> 2 -> 1 -> 5 -> 7}
+        //System.out.println(tree.maxDiameter(root));   // 6 Diameter = {6 -> 4 -> 2 -> 1 -> 5 -> 7}
         //tree.flattenInOrder(root);                    // 3 <-> 2 <-> 9 <-> 4 <-> 1 <-> 6 <-> 5 <-> 7 <-> null
         //tree.lowestCommonAncestor(root, 9, 3)         // returns Node 2
         //tree.verticalOrder(root);                     // 3 2 9 1 4 6 5 7
+        //bst.floorAndCeilArrayList(bstRoot, 16);       // Floor: 15 Ceil: 17
+        //bst.floorAndCeil(bstRoot, 16);                // Floor: 15 Ceil: 17
     }
 }
