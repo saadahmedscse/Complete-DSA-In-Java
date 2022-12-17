99be474f8a04ff7d94a1bf8cd8b054ba8a0abc9c
saada <saadahmedscse@gmail.com>
2022-12-17 16:52:46 +0600

    Added BFS Traversal

--- Complete DSA/Graph/Graph.java
+++ Complete DSA/Graph/Graph.java
@@ -16,4 +16,30 @@ public class Graph {
     public void addEdge(int s, int d) {
         adj.get(s).add(d);
     }
+
+    public void bfsTraversal(int source) {
+        List<Integer> result = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[v];
+        Arrays.fill(visited, false);
+
+        q.offer(source);
+        visited[source] = true;
+
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            result.add(curr);
+
+            for (int i = 0; i < adj.get(curr).size(); i++) {
+                int neighbor = adj.get(curr).get(i);
+
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    q.offer(neighbor);
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
 }
