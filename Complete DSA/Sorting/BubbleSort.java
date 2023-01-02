cf5e274a5d88bd950d02759df6276ba65fc25b25
saada <saadahmedscse@gmail.com>
2023-01-02 12:27:17 +0600

    1. Bubble Sort Function

--- Complete DSA/Sorting/BubbleSort.java
+++ Complete DSA/Sorting/BubbleSort.java
@@ -15,4 +15,18 @@ public class BubbleSort {
             }
         }
     }
+
+    public static void bubbleSortDesc(int[] nums) { // TC O(N ^ 2) ___ SC O(1)
+        int n = nums.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (nums[j] < nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+    }
 }
