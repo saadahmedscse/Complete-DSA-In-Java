f0d1fa50326ed3584921b63f3a46425690625915
saada <saadahmedscse@gmail.com>
2023-01-02 13:59:07 +0600

    3. Selection Sort Function

--- Complete DSA/Sorting/SelectionSort.java
+++ Complete DSA/Sorting/SelectionSort.java
@@ -5,9 +5,9 @@ public class SelectionSort {
     public static void selectionSort(int[] nums) { // TC O(N ^ 2) ___ SC O(1)
         int n = nums.length;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n - 1; i++) {
             int min = i;
-            int j = i;
+            int j = i + 1;
 
             while (j < n) {
                 if (nums[j] < nums[min]) {
@@ -27,9 +27,9 @@ public class SelectionSort {
     public static void selectionSortDesc(int[] nums) { // TC O(N ^ 2) ___ SC O(1)
         int n = nums.length;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n - 1; i++) {
             int max = i;
-            int j = i;
+            int j = i + 1;
 
             while (j < n) {
                 if (nums[j] > nums[max]) {
