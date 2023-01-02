0a1ff94607ac5e374dc958efd51723172015aea8
saada <saadahmedscse@gmail.com>
2023-01-02 12:30:30 +0600

    2. Insertion Sort Function

---
+++ Complete DSA/Sorting/InsertionSort.java
@@ -0,0 +1,19 @@
+package Sorting;
+
+public class InsertionSort {
+    
+    public static void insertionSort(int[] nums) { // TC O(N ^ 2) ___ SC O(1)
+        int n = nums.length;
+
+        for (int i = 1; i < n; i++) {
+            int j = i - 1;
+            int temp = nums[i];
+
+            while (j >= 0 && nums[j] > temp) {
+                nums[j + 1] = nums[j--];
+            }
+
+            nums[j + 1] = temp;
+        }
+    }
+}
