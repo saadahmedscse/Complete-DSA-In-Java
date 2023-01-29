75051a7e4d847017e0b93b790e1888577c1364ae
Saad <saadahmedscse@gmail.com>
2023-01-29 19:46:19 +0600

    AVL Binary tree implementation

--- BinaryTree/AVLTree/BinaryTree.java
+++ BinaryTree/AVLTree/BinaryTree.java
@@ -52,6 +52,56 @@ public class BinaryTree {
         return root;
     }
 
+    /**
+     * Time Complexity [delete()] function:
+     * O(LogN)
+     */
+
+    public Node delete(Node root, int data) {
+        if (root == null) return null;
+
+        if (data < root.data) root.left = delete(root.left, data);
+        else if (data > root.data) root.right = delete(root.right, data);
+        else {
+            if (root.left == null || root.right == null) {
+                return root.left == null ? root.right : root.left;
+            }
+            else {
+                int min = minValue(root.right);
+                root.data = min;
+                root.right = delete(root.right, min);
+            }
+        }
+
+        root.height = 1 + max(height(root.left), height(root.right));
+
+        int balance = getBalance(root);
+
+        if (balance > 1 && getBalance(root.left) >= 0) return rightRotate(root);
+        if (balance > 1 && getBalance(root.left) < 0) {
+            root.left = leftRotate(root.left);
+            return rightRotate(root);
+        }
+        if (balance < -1 && getBalance(root.right) >= 0) return leftRotate(root);
+        if (balance < -1 && getBalance(root.right) < 0) {
+            root.right = rightRotate(root.right);
+            return leftRotate(root.left);
+        }
+
+        return root;
+    }
+
+    private int minValue(Node root) {
+        int min = root.data;
+
+        while (root != null) {
+            min = root.data;
+            root = root.left;
+        }
+
+        return min;
+    }
+
     private Node rightRotate(Node x) {
         Node y = x.left;
         Node z = y.right;
