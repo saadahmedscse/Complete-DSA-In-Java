54eff7e35ec9ad54e7056f228e1cfdb74c0be430
saada <saadahmedscse@gmail.com>
2023-01-30 16:40:51 +0600

    Graph DFS Traversal Implementation of Adjacency List

---
+++ Graph/Traversal/AdjacencyList/DFSTraversal.java
@@ -0,0 +1,72 @@
+package Graph.Traversal.AdjacencyList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DFSTraversal {
+
+    /**
+     * Graph DFS Traversal Implementation of Adjacency List
+     * Author: Saad Ahmed
+     * Date: 30 Jan, 2023
+     */
+
+    /**
+     * Complexity [traverse()] function:
+     * Time Complexity:  O(V + E)
+     * Space Complexity: O(V)
+     */
+
+    public static void traverse(List<List<Integer>> graph, int src, int V) {
+        List<Integer> result = new ArrayList<>();
+        dfs(graph, src, new boolean[V], result);
+
+        System.out.println(result);
+    }
+
+    private static void dfs(List<List<Integer>> graph, int src, boolean[] visited, List<Integer> result) {
+        result.add(src);
+        visited[src] = true;
+
+        for (int neighbor : graph.get(src)) {
+            if (!visited[neighbor]) {
+                dfs(graph, neighbor, visited, result);
+            }
+        }
+    }
+
+    /**
+     * Complexity [traverseWholeGraph()] function:
+     * Time Complexity:  O(V + E)
+     * Space Complexity: O(V)
+     */
+
+    public static void traverseWholeGraph(List<List<Integer>> graph, int V) {
+        boolean[] visited = new boolean[V];
+        List<List<Integer>> result = new ArrayList<>();
+        int totalComponents = 0;
+
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                totalComponents++;
+                result.add(dfsWhole(graph, i, visited, new ArrayList<>()));
+            }
+        }
+
+        System.out.println(result);
+        System.out.println("Total Components: " + totalComponents);
+    }
+
+    private static List<Integer> dfsWhole(List<List<Integer>> graph, int src, boolean[] visited, List<Integer> current) {
+        current.add(src);
+        visited[src] = true;
+
+        for (int neighbor : graph.get(src)) {
+            if (!visited[neighbor]) {
+                dfsWhole(graph, neighbor, visited, current);
+            }
+        }
+
+        return current;
+    }
+}
