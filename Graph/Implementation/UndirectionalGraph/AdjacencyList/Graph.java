73da0fe4e746bbe9a684e20f787d4896704a2047
saada <saadahmedscse@gmail.com>
2023-01-30 15:52:21 +0600

    Undirectional Graph Implementation using Adjacency List

---
+++ Graph/Implementation/UndirectionalGraph/AdjacencyList/Graph.java
@@ -0,0 +1,35 @@
+package Graph.Implementation.UndirectionalGraph.AdjacencyList;
+
+import java.util.List;
+
+import java.util.ArrayList;
+
+public class Graph {
+
+    /**
+     * Undirectional Graph Implementation using Adjacency List
+     * Author: Saad Ahmed
+     * Date: 30 Jan, 2023
+     */
+    
+    private final List<List<Integer>> graph;
+
+    public Graph(int V) {
+        graph = new ArrayList<>();
+
+        for (int i = 0; i < V; i++) graph.add(new ArrayList<>());
+    }
+
+    public void addEdge(int src, int dest) {
+        graph.get(src).add(dest);
+        graph.get(dest).add(src);
+    }
+
+    public List<List<Integer>> getGraph() {
+        return graph;
+    }
+
+    public String toString() {
+        return graph.toString();
+    }
+}
